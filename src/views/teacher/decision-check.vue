aa0d7b3cfba79435343cb5efcc1c6a08c8dd8288
wangtanke <1665497618@qq.com>
2020-03-05 16:37:09 +0800

    teacher content

--- src/views/teacher/decision-check.vue
+++ src/views/teacher/decision-check.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import formItem from "../../components/content/form-block";
+import limit from "../../components/content/limit-number";
+import { Obj2html } from "../../command/utils";
 export default {
   
 }

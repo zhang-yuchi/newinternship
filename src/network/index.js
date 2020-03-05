0dd495ee6a07bbf63dad3a7de3f70fcf166401c9
zhangyuchi <2669327287@qq.com>
2020-03-05 21:26:26 +0800

    修改

--- src/network/index.js
+++ src/network/index.js
@@ -131,7 +131,7 @@ export const getStudentList = ()=>{
     return service.get(getRandom('/teacher/students'))
 }
 //获取某个学生的信息
-export const getStudentIdentify = (stuId) => {
+export const getStudentInfo = (stuId) => {
     return service.get(getRandom('/teacher/student/'+stuId))
 }
 //获取某个学生鉴定表

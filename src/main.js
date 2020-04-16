7ab5ca5753202d415bde4775921b4ee362cc0fd0
wangtanke <1665497618@qq.com>
2020-04-16 17:24:16 +0800

    增加error捕捉

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import 'babel-polyfill'
 //引入element-ui
-import ElementUI from 'element-ui'
+import ElementUI, { Form } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css';
 import './assets/css/hidden.css'
@@ -17,3 +17,26 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
+// Promise内全局异常捕获
+window.addEventListener('unhandledrejection',e=>{
+  e.preventDefault()
+  console.log('log-error:::\n'+e.reason.stack)
+  //  
+  return true
+})
+
+// window error捕获的错误
+window.onerror = (msg, url, line, col, error) => {
+  console.log('log-error:::\n'+msg+',发生在:'+url,'第'+line+'行')
+  //  发给后台
+  //  
+  return true // error不会以error形式打印到控制台
+}
+// vue捕获的错误不会上报到window.error
+Vue.config.errorHandler = function (error, vm, msg) {
+  let e = 'log-error:::\n'+error+'in：'+msg+vm
+  // 发给后台
+
+}

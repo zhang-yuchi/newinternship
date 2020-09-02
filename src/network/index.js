1f3a9155dfae2ef9691b1e0a55378ec9d2310558
zhangyuchi <2669327287@qq.com>
2020-09-02 11:50:11 +0800

    mdf network dev

--- src/network/index.js
+++ src/network/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import qs from 'qs'
 import vm from 'element-ui'
 let isDev = process.env.NODE_ENV === 'development'
-isDev = false
+// isDev = false
 const baseURL = isDev ? "http://hz.nadev.cn:8255/internship" : "http://sx.cdcas.edu.cn:8890/internship"
 const service = axios.create({
   baseURL

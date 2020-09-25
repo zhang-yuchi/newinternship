b5a00864ae99154ae7bdcf185931c1bd14b40347
wangtanke <1665497618@qq.com>
2020-09-25 17:27:29 +0800

    del errorLog

--- src/main.js
+++ src/main.js
@@ -12,30 +12,6 @@ Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
-import { errorLog } from './network/logerror'
-// Promise内全局异常捕获
-window.addEventListener('unhandledrejection', e => {
-  e.preventDefault()
-  let str = `error::: ${e.reason.stack}`
-  console.log(str)
-  errorLog(str)
-  return true
-})
-
-// window error捕获的错误
-window.onerror = (msg, url, line, col, error) => {
-  let str = `error::: ${msg} 发生在: ${url} 第${line}行`
-  console.log(str)
-  errorLog(str)
-  return true // error不会以error形式打印到控制台
-}
-// vue捕获的错误不会上报到window.error
-Vue.config.errorHandler = function (error, vm, msg) {
-  let str = `error::: ${error} 发生在组件：${vm.$options._componentTag} 的 ${msg} 中`
-  console.log(str)
-  errorLog(str)
-}
-
 new Vue({
   router,
   store,
